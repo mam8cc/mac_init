8fd4311fd26b6e7d9257941093325b1d0e42aa96
Mike McClannahan <mam8cc@users.noreply.github.com>
2017-04-07 13:37:02 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,42 @@
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
+#!/bin/bash
 
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python get-pip.py
-pip install ansible
-ansible-galaxy install icopp.homebrew
-brew update
-brew install python3
+
+install_pip() {
+  if hash pip 2>/dev/null; then {
+    echo "pip already installed... skipping"
+  }
+  else {
+    curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+    sudo python get-pip.py
+  }
+  fi
+}
+
+install_ansible() {
+  if hash ansible-galaxy 2>/dev/null; then {
+    echo "ansible-galaxy already installed... skipping"
+  }
+  else {
+    pip install ansible
+  }
+  fi
+}
+
+install_homebrew() {
+  if hash brew 2>/dev/null; then {
+    echo "homebrew already installed... skipping"
+  }
+  else {
+    ansible-galaxy install icopp.homebrew
+    brew update
+  }
+  fi
+}
+
+install_pip
+install_ansible
+install_homebrew
+#pip install ansible
+#ansible-galaxy install icopp.homebrew
+#brew update
+#brew install python3
